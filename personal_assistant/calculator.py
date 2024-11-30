ccf250859ec6ed47023ff1a6b36b55f03710f789
ScoChillOFF <cheatkiller12@gmail.com>
2024-11-30 21:43:42 +0300

    Added calculator

---
+++ personal_assistant/calculator.py
@@ -0,0 +1,15 @@
+class Calculator:
+    def __init__(self):
+        self.allowed_operators = ["+", "-", "*", "/", "(", ")"]
+
+    def calculate(self, expression):
+        if all(char.isdigit() or char.isspace() or char in self.allowed_operators for char in expression):
+            try:
+                result = eval(expression)
+                print(result)
+            except ZeroDivisionError:
+                print("Ошибка: деление на ноль")
+            except Exception as e:
+                print(f"Ошибка: {str(e)}")
+        else:
+            return "Ошибка: недопустимые символы в выражении"
